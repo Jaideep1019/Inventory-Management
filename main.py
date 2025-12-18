e73549d659dc87663bf88ff3e082c64ed7f4c7c8
Jaideep1019 <jaideepreddy1019@gmail.com>
2025-12-18 19:30:12 +0530

    Final Changes

--- main.py
+++ main.py
@@ -1,35 +1,59 @@
-from store.products import get_all_products,add_product,update_product,record_sale,delete_product,view_product_details
+from store.products import get_all_products,add_product,update_product_quantity,record_sale,update_records,delete_product,view_product_details,check_reoder_alerts,generate_inventory_report
 from store.supply import add_supplier,get_supplier_products
+
+# 1. Add New Product #complete
+# 2. Update Product Stock #complete
+# 3. Record a Sale #complete
+# 4. Add Supplier #complete
+# 5. View Supplier's Products #complete 
+# 6. Generate Inventory Report #complete
+# 7. Check Reorder Alerts #complete
+# 8. View Product Details #complete
+# 9. Delete a Product #complete
+# 10. Exit
+# 11. View All product Details #complete
+
 def main():
-    print('''
-          
-1. Add New Product #complete
-2. Update Product Stock #complete
-3. Record a Sale #complete
-4. Add Supplier #complete
-5. View Supplier's Products #complete 
+    print('''       
+1. Add New Product
+2. Update Product Stock
+3. Record a Sale
+4. Add Supplier
+5. View Supplier's Products
 6. Generate Inventory Report
 7. Check Reorder Alerts
-8. View Product Details #complete
-9. Delete a Product #complete
+8. View Product Details
+9. Delete a Product
 10. Exit
 11. View All product Details
-
 ''')
     while True:
+#         print('''         
+# 1. Add New Product
+# 2. Update Product Stock
+# 3. Record a Sale
+# 4. Add Supplier
+# 5. View Supplier's Products
+# 6. Generate Inventory Report
+# 7. Check Reorder Alerts
+# 8. View Product Details
+# 9. Delete a Product
+# 10. Exit
+# 11. View All product Details
+# ''')
         choice = int(input('Enter an option: '))
         if choice == 1:
             p_id = int(input('Enter product id: '))
             p_category = input('Enter product category: ')
             p_name = input('Enter product name: ')
             p_price = int(input('Enter product price: '))
-            p_quantity = input('Enter no.of products: ')
+            p_quantity = int(input('Enter no.of products: '))
             a = add_product(p_id,p_category,p_name,p_price,p_quantity)
             print(a)
         elif choice == 2:
             p_id = int(input('Enter product id: '))
             u_quantity = int(input('Enter no.of products to update: '))
-            b = update_product(p_id,u_quantity)
+            b = update_product_quantity(p_id,u_quantity)
             print(b)
         elif choice == 3:
             p_id = int(input('Enter product id: '))
@@ -38,7 +62,9 @@ def main():
             h = c.copy() # create a copy of returned dictionary and change the quantity
             h.pop('quantity')
             h['quantity']=u_quantity
+            i = update_records(h)
             print(h)
+            print(i)
         elif choice == 4:
             s_id = int(input('Enter Supplier ID: '))
             s_name = input('Enter Supplier Name: ')
@@ -51,9 +77,14 @@ def main():
             print(f'Products of Supplier ID {s_id}')
             print(g)
         elif choice == 6:
-            pass
+            j = generate_inventory_report()
+            print(j)
         elif choice == 7:
-            pass
+            k = check_reoder_alerts()
+            if k != []:
+                print(k)
+            else:
+                print('All products are available')
         elif choice == 8:
             p_id = int(input('Enter the Product ID: '))
             f = view_product_details(p_id)
--- store/products.py
+++ store/products.py
@@ -17,15 +17,16 @@ products = [
         'id':2,
         'category':'clothes',
         'name':'Brown Jeans',
+        'size':'S,M,L,XL',
         'price':899,
-        'quantity':30
+        'quantity':40
     },
     {
         'id':3,
         'category':'spectacles',
         'name':'Raybon Polarized oval glasses',
         'price':10_999,
-        'quantity':14
+        'quantity':15
     }
 ]
 
@@ -49,12 +50,18 @@ def add_product(p_id,p_category,p_name,p_price,p_quantity):
     )
     return 'Product Added Successfully'
 
-def update_product(p_id,u_quantity):
+def update_product_quantity(p_id,u_quantity):
     for p in products:
         if p['id'] == p_id:
             p['quantity'] += u_quantity
             return p
     return 'Invalid product ID'
+
+records = []
+def update_records(h):
+    records.append(h)
+    return 'Product sale is recorded' 
+#f'Product sale is recorded {records}' - code for verifying records list
 
 def record_sale(p_id,u_quantity):
     for p in products:
@@ -73,5 +80,21 @@ def delete_product(p_id):
 
 def view_product_details(p_id):
     for p in products:
-        if p['id'] == p_id:
+        if p.get('id') == p_id:
             return p
+
+alerts = []
+def check_reoder_alerts():
+    for p in products:
+        if p['quantity']<=10:
+            alerts.append(f'alert "{p['name']}" is low in quantity')
+    return alerts
+
+# calculating all the products sold by referring sales record 
+def generate_inventory_report():
+    total_revenue = 0
+    total_products_sold = 0
+    for r in records:
+        total_revenue += (r['price']*r['quantity'])
+        total_products_sold += r['quantity']
+    return f'Total Products Sold = {total_products_sold} and Total Revenue generated = {total_revenue}'
